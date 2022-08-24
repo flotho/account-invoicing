72f460b9c0f2447892e82911d1d8a1570af3a108
Florent THOMAS <florent.thomas@mind-and-go.com>
2022-08-24 17:17:03 +0200

    [FIX][14.0]: bad tax computation
    FIX #1107 for v14.0

--- account_invoice_fixed_discount/models/account_move.py
+++ account_invoice_fixed_discount/models/account_move.py
@@ -23,6 +23,14 @@ class AccountMove(models.Model):
         for line in vals.keys():
             line.update(vals[line])
         return res
+    
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        if any([l.discount_fixed for l in res.invoice_line_ids ] ):
+            res.with_context(check_move_validity=False)._recompute_tax_lines()
+            res.with_context(check_move_validity=False)._onchange_invoice_line_ids()
+        return res
 
 
 class AccountMoveLine(models.Model):
